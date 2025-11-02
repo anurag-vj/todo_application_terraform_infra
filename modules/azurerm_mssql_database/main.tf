0d9f3809911162f8fed5d61836eb60ced8dc2180
Anurag Vijay <anuxperia.c@gmail.com>
2025-11-02 11:28:00 +0530

    terraform code changes

--- modules/azurerm_mssql_database/main.tf
+++ modules/azurerm_mssql_database/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mssql_database" "mssql_database" {
   for_each     = var.mssql_database
   name         = each.value.db_name
-  server_id    = data.azurerm_sql_server.sql_server.id
+  server_id    = data.azurerm_mssql_server.sql_server.id
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
   max_size_gb  = 2
